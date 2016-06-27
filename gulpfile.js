d6b47759efb4ad0a25babd88bdb0ea3968047f4b
guduziye <05123875lgh@163.com>
2016-06-27 19:21:20 +0800

    调整目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var cssFiles = [
 
 var cssConfig = {
     prefixerScheme: ['> 1%', 'last 2 versions', 'Android >= 4.0', 'iOS >= 8'],
-    writePath: 'build/css/',
+    writePath: 'css/',
     onsenFileName: 'onsenui',
     onsenComponentsFileName: 'onsen-css-components',
     bhComponentsFileName: 'bh-components',
@@ -38,25 +38,24 @@ gulp.task('browser-sync', function () {
 
 gulp.task('js', function(){
     gulp.src('./src/js/*.js')
-        .pipe(gulp.dest('./build/js/'))
+        .pipe(concat('bhtc-datetimepicker.js'))
+        .pipe(gulp.dest('./js/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('./build/js/'));
+        .pipe(gulp.dest('./js/'));
 });
 
 gulp.task('css', function(){
     for(var i=0, cssFilesLen = cssFiles.length; i<cssFilesLen; i++){
         var skinName = cssFiles[i];
-        var writeFilePath = './build/css/'+skinName;
+        var writeFilePath = './css/'+skinName;
         gulp.src(['./src/sass/skins/'+skinName+'/color.scss',
             './src/sass/*.scss'])
-            .pipe(concat('bootstrap-datetimepicker.scss'))
+            .pipe(concat('bhtc-datetimepicker.scss'))
             .pipe(sass().on('error', sass.logError))
             .pipe(autoprefixer({
                 browsers: cssConfig.prefixerScheme
             }))
-            .pipe(gulp.dest(writeFilePath))
-            .pipe(rename('bootstrap-datetimepicker.css'))
             .pipe(gulp.dest(writeFilePath))
             .pipe(rename({suffix: '.min'}))
             .pipe(minifycss())
@@ -73,7 +72,8 @@ gulp.task('watch', function () {
 gulp.task('default',['watch', 'js', 'css', 'browser-sync'], function(){
     gulp.watch([
         'example/**/*.{js,css,html}',
-        'build/**/*.{js,css,html}'
+        'js/**/*.js',
+        'css/**/*.css'
     ]).on('change', function (changedFile) {
         gulp.src(changedFile.path)
             .pipe(browserSync.reload({
